b6a9a18a42d1811ee8c4e24839a861ffcd2204ce
Roberto Quezada <sgtquezada@gmail.com>
2015-12-18 05:34:07 +0000

    added model association and form validation

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,5 +1,29 @@
 class WishesController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+
     def index
         @wishes = Wish.page(params[:page]).per(4)
+    end
+
+    def new
+        @wish = Wish.new
+    end
+
+    def create
+      @wish = current_user.wish.create(place_params)
+      if @wish.valid?
+        redirect_to root_path
+      else
+        render :new, :status => :unprocessable_entity
+      end
+    end
+
+    def show
+      @wish = Wish.find(params[:id])
+    end
+
+
+    def place_params
+      params.require(:wish).permit(:wish, :name)
     end
 end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,2 +1,7 @@
 class Wish < ActiveRecord::Base
+  belongs_to :user
+
+  # form validation
+  validates :name, :presence => true
+  validates :wish, :presence => true
 end
